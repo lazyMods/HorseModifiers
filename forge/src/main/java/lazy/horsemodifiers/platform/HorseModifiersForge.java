d9ebb88c5af570e0820b00de7ac61d8b17f3958d
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-06-25 23:57:19 +0100

    All done on the forge side

--- forge/src/main/java/lazy/horsemodifiers/platform/HorseModifiersForge.java
+++ forge/src/main/java/lazy/horsemodifiers/platform/HorseModifiersForge.java
@@ -1,12 +1,14 @@
-package lazy.horsemodifiers.forge;
+package lazy.horsemodifiers.platform;
 
 import lazy.horsemodifiers.HorseModifiers;
+import lazy.horsemodifiers.Ref;
 import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-@Mod(HorseModifiers.MOD_ID)
+@Mod(Ref.MOD_ID)
 public class HorseModifiersForge {
+
     public HorseModifiersForge() {
         EventBuses.registerModEventBus(HorseModifiers.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         HorseModifiers.init();
