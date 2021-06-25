b090a98e2612edd1b827258514a51e04f62a36fe
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-06-26 00:50:11 +0100

    Added forge data gen

---
+++ forge/src/main/java/lazy/horsemodifiers/platform/data/DataGenerator.java
@@ -0,0 +1,16 @@
+package lazy.horsemodifiers.platform.data;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerator {
+
+    @SubscribeEvent
+    public static void onGatherData(GatherDataEvent event) {
+        if (event.includeServer()) {
+            event.getGenerator().addProvider(new RecipeGenerator(event.getGenerator()));
+        }
+    }
+}
