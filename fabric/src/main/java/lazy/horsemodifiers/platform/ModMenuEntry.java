76d8f22f6f66db5916050ec0787a21952d442460
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-06-26 16:38:11 +0100

    forgot to add the modmenu entrypoint

--- fabric/src/main/java/lazy/horsemodifiers/platform/ModMenuEntry.java
+++ fabric/src/main/java/lazy/horsemodifiers/platform/ModMenuEntry.java
@@ -5,6 +5,7 @@ import io.github.prospector.modmenu.api.ModMenuApi;
 import lazy.horsemodifiers.screen.ConfigScreen;
 
 public class ModMenuEntry implements ModMenuApi {
+
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return ConfigScreen::create;
