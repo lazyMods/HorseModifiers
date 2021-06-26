f3e33ec9200bda05a3afdec8bd5e34f156a32c0b
lazynessmind <52864251+lazynessmind@users.noreply.github.com>
2021-06-26 14:58:11 +0100

    Added Cloth ConfigScreen

--- forge/src/main/java/lazy/horsemodifiers/platform/HorseModifiersForge.java
+++ forge/src/main/java/lazy/horsemodifiers/platform/HorseModifiersForge.java
@@ -1,8 +1,12 @@
 package lazy.horsemodifiers.platform;
 
 import lazy.horsemodifiers.HorseModifiers;
+import lazy.horsemodifiers.screen.ConfigScreen;
 import lazy.horsemodifiers.util.Ref;
 import me.shedaniel.architectury.platform.forge.EventBuses;
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -12,5 +16,7 @@ public class HorseModifiersForge {
     public HorseModifiersForge() {
         EventBuses.registerModEventBus(Ref.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         HorseModifiers.init();
+        if(ModList.get().isLoaded("cloth-config"))
+            ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (client, parent) -> ConfigScreen.create(parent));
     }
 }
